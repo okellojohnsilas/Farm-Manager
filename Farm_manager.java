45096665c1a4d4dd6d0a530c7b4e89c17f279fa0
okellojohnsilas <okello.john@strathmore.edu>
2019-11-17 14:51:56 +0300

    Add SHA salted algorithm in PasswordHashing class and add to the project

--- Farm_manager.java
+++ Farm_manager.java
@@ -11,11 +11,11 @@ import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import org.apache.commons.lang3.RandomStringUtils;
 
-public class Farm_manager extends Application{
-   @Override     
-   public void start(Stage primarystage) throws Exception { 
+public class Farm_manager extends Application {
+
+    @Override
+    public void start(Stage primarystage) throws Exception {
 //      ========================BUY ANIMAL INTERFACE==============================================================  
 //      Text displayed
         Text firstusermessage = new Text("");
@@ -35,15 +35,15 @@ public class Farm_manager extends Application{
 //      ComboBox Section
 //      Gender ComboBox
         ComboBox a_genderComboBox = new ComboBox();
-        a_genderComboBox.getItems().addAll("Male","Female");
+        a_genderComboBox.getItems().addAll("Male", "Female");
         a_genderComboBox.setEditable(false);
 //        Horntype ComboBox
         ComboBox h_typeComboBox = new ComboBox();
-        h_typeComboBox.getItems().addAll("Short","Long","None");
+        h_typeComboBox.getItems().addAll("Short", "Long", "None");
         h_typeComboBox.setEditable(true);
 //        Agegroup ComboBox
         ComboBox a_ageGroupComboBox = new ComboBox();
-        a_ageGroupComboBox.getItems().addAll("Mature","Calf","Youngling");
+        a_ageGroupComboBox.getItems().addAll("Mature", "Calf", "Youngling");
         a_ageGroupComboBox.setEditable(true);
 //      Creating the Buttons
         Button buyAnimalButton = new Button("BUY");
@@ -51,9 +51,9 @@ public class Farm_manager extends Application{
 //      Creating the GridPane
         GridPane initialgridpane = new GridPane();
 //      Setting up size of Grid pane
-        initialgridpane.setMinSize(600,400);
+        initialgridpane.setMinSize(600, 400);
 //      Setting the Padding
-        initialgridpane.setPadding(new Insets(10,10,10,10));
+        initialgridpane.setPadding(new Insets(10, 10, 10, 10));
 //      Setting the vertical and horizontal gaps between the columns
         initialgridpane.setVgap(10);
         initialgridpane.setHgap(10);
@@ -61,24 +61,24 @@ public class Farm_manager extends Application{
         initialgridpane.setAlignment(Pos.CENTER);
 //      Arranging all the nodes on the grid
         initialgridpane.add(a_breed, 0, 0);
-        initialgridpane.add(a_breedTextField,1, 0);
+        initialgridpane.add(a_breedTextField, 1, 0);
         initialgridpane.add(a_gender, 0, 2);
-        initialgridpane.add(a_genderComboBox,1, 2);
+        initialgridpane.add(a_genderComboBox, 1, 2);
         initialgridpane.add(a_color, 0, 4);
-        initialgridpane.add(a_colorTextField,1, 4);
+        initialgridpane.add(a_colorTextField, 1, 4);
         initialgridpane.add(h_type, 0, 6);
-        initialgridpane.add(h_typeComboBox,1, 6);
+        initialgridpane.add(h_typeComboBox, 1, 6);
         initialgridpane.add(a_ageGroup, 0, 8);
-        initialgridpane.add(a_ageGroupComboBox,1, 8);
+        initialgridpane.add(a_ageGroupComboBox, 1, 8);
 //        initialgridpane.add(a_picture, 0, 10);
 //        initialgridpane.add(filechoice,1, 10);
         initialgridpane.add(a_price, 0, 10);
-        initialgridpane.add(a_priceTextField,1, 10);
+        initialgridpane.add(a_priceTextField, 1, 10);
         initialgridpane.add(tagNum, 0, 12);
         initialgridpane.add(tagNumTextField, 1, 12);
-        initialgridpane.add(firstusermessage,1, 14);
-        initialgridpane.add(buyAnimalButton,1, 16);
-        initialgridpane.add(salesAnimalButton,2, 16);
+        initialgridpane.add(firstusermessage, 1, 14);
+        initialgridpane.add(buyAnimalButton, 1, 16);
+        initialgridpane.add(salesAnimalButton, 2, 16);
         buyAnimalButton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill:white; -fx-font-size:13pt;");
         salesAnimalButton.setStyle("-fx-background-color: darkslateblue; -fx-text-fill:white; -fx-font-size:13pt;");
         initialgridpane.setStyle("-fx-background-color:BEIGE;");
@@ -88,7 +88,7 @@ public class Farm_manager extends Application{
 //        The text to be displayed
         Text Secondusermessage = new Text("");
         Text secondtagNum = new Text("TAG NUMBER: ");
-        Text animalBuyer = new Text("BUYER: ");   
+        Text animalBuyer = new Text("BUYER: ");
         Text amountPaid = new Text("AMOUNT PAID: ");
         Text buyerContact = new Text("BUYER CONTACT: ");
 //      Creating the TextFields
@@ -102,9 +102,9 @@ public class Farm_manager extends Application{
 //      Instatiating the Gridpane
         GridPane salesgridpane = new GridPane();
 //      Setting up size of Grid pane
-        salesgridpane.setMinSize(600,400);
+        salesgridpane.setMinSize(600, 400);
 //      Setting the Padding
-        salesgridpane.setPadding(new Insets(10,10,10,10));
+        salesgridpane.setPadding(new Insets(10, 10, 10, 10));
 //      Setting the vertical and horizontal gaps between the columns
         salesgridpane.setVgap(10);
         salesgridpane.setHgap(10);
@@ -130,9 +130,9 @@ public class Farm_manager extends Application{
 //        Adding action events
 //        Animal related buttons
         Animal cow = new Animal();
-        buyAnimalButton.setOnAction((e) ->{
+        buyAnimalButton.setOnAction((e) -> {
             try {
-                cow.purchaseAnimal(tagNumTextField.getText(),a_breedTextField.getText(),a_genderComboBox.getValue().toString(),a_colorTextField.getText(),h_typeComboBox.getValue().toString(),a_ageGroupComboBox.getValue().toString(),a_priceTextField.getText());
+                cow.purchaseAnimal(tagNumTextField.getText(), a_breedTextField.getText(), a_genderComboBox.getValue().toString(), a_colorTextField.getText(), h_typeComboBox.getValue().toString(), a_ageGroupComboBox.getValue().toString(), a_priceTextField.getText());
                 tagNumTextField.clear();
                 a_breedTextField.clear();
                 a_colorTextField.clear();
@@ -144,9 +144,9 @@ public class Farm_manager extends Application{
                 Logger.getLogger(Farm_manager.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
-        saleAnimalButton.setOnAction((e) ->{
+        saleAnimalButton.setOnAction((e) -> {
             try {
-                cow.saleAnimal(secondtagNumTextField.getText(),buyerTextField.getText(),buyerContactTextField.getText(),salespriceTextField.getText());
+                cow.saleAnimal(secondtagNumTextField.getText(), buyerTextField.getText(), buyerContactTextField.getText(), salespriceTextField.getText());
             } catch (ClassNotFoundException | SQLException ex) {
                 Logger.getLogger(Farm_manager.class.getName()).log(Level.SEVERE, null, ex);
             }
@@ -157,10 +157,130 @@ public class Farm_manager extends Application{
         backButton.setOnAction((e) -> {
             primarystage.setScene(testScene);
         });
-        primarystage.setScene(testScene);
+        Users farmer = new Users();
+//      ========================ADD USER INTERFACE=================================================================================
+//      Texts
+        Text adduserFirstNameText = new Text("FIRST NAME");
+        Text adduserLastNameText = new Text("LAST NAME");
+        Text adduserUsernameText = new Text("USERNAME");
+        Text adduseruserTypeText = new Text("USERTYPE");
+        Text adduseruser_PasswordText = new Text("PASSWORD");
+        Text adduserConfirmUser_PasswordText = new Text("CONFIRM PASSWORD");
+        Text adduserTelephoneText = new Text("TELEPHONE CONTACT(e.g +256 123....)");
+        Text adduserFarmNameText = new Text("FARM NAME");
+//      TextFields
+        TextField adduserfirstNameTextField = new TextField();
+        TextField adduserlastNameTextField = new TextField();
+        TextField adduserUserNameTextField = new TextField();
+        TextField addusertelephoneNumberTextField = new TextField();
+        TextField adduserfarmNameTextField = new TextField();
+//      Combo Box
+        ComboBox userTypeComboBox = new ComboBox();
+        userTypeComboBox.getItems().addAll("Admin", "User");
+        userTypeComboBox.setEditable(false);
+//      PasswordField
+        PasswordField newUserPassword = new PasswordField();
+        PasswordField ConfirmNewUserPassword = new PasswordField();
+//      Buttons
+        Button addUser_Button = new Button("ADD");
+//      Creating new GridPane
+        GridPane addUserGridpane = new GridPane();
+//      Setting up size of Grid pane
+        addUserGridpane.setMinSize(600, 400);
+//      Setting the Padding
+        addUserGridpane.setPadding(new Insets(10, 10, 10, 10));
+//      Setting the vertical and horizontal gaps between the columns
+        addUserGridpane.setVgap(10);
+        addUserGridpane.setHgap(10);
+//      Setting the grid alignment
+        addUserGridpane.setAlignment(Pos.CENTER);
+//      Arranging all the nodes on the gridFFFFFF
+        addUserGridpane.add(adduserFirstNameText, 0, 0);
+        addUserGridpane.add(adduserLastNameText, 0, 2);
+        addUserGridpane.add(adduseruserTypeText, 0, 4);
+        addUserGridpane.add(adduserUsernameText, 0, 6);
+        addUserGridpane.add(adduseruser_PasswordText, 0, 8);
+        addUserGridpane.add(adduserConfirmUser_PasswordText, 0, 10);
+        addUserGridpane.add(adduserTelephoneText, 0, 12);
+        addUserGridpane.add(adduserFarmNameText, 0, 14);
+        addUserGridpane.add(adduserfirstNameTextField, 1, 0);
+        addUserGridpane.add(adduserlastNameTextField, 1, 2);
+        addUserGridpane.add(userTypeComboBox, 1, 4);
+        addUserGridpane.add(adduserUserNameTextField, 1, 6);
+        addUserGridpane.add(newUserPassword, 1, 8);
+        addUserGridpane.add(ConfirmNewUserPassword, 1, 10);
+        addUserGridpane.add(addusertelephoneNumberTextField, 1, 12);
+        addUserGridpane.add(adduserfarmNameTextField, 1, 14);
+        addUserGridpane.add(addUser_Button, 1, 16);
+        Scene addUserScene = new Scene(addUserGridpane);
+        addUser_Button.setOnAction((e) -> {
+            if (newUserPassword.getText() == null ? ConfirmNewUserPassword.getText() == null : newUserPassword.getText().equals(ConfirmNewUserPassword.getText())) {
+                farmer.AddUser(adduserfirstNameTextField.getText(), adduserlastNameTextField.getText(), adduserUserNameTextField.getText(), userTypeComboBox.getValue().toString(), newUserPassword.getText(), addusertelephoneNumberTextField.getText(), adduserfarmNameTextField.getText());
+                adduserfirstNameTextField.clear();
+                adduserlastNameTextField.clear();
+                adduserUserNameTextField.clear();
+                newUserPassword.clear();
+                addusertelephoneNumberTextField.clear();
+                userTypeComboBox.getSelectionModel().clearSelection();
+                adduserfarmNameTextField.clear();
+                primarystage.setScene(testScene);
+            } else {
+                System.out.println("The passwords didnt match");
+            }
+        });
+//      ========================END OF ADD USER INTERFACE==========================================================================
+//      ========================LOGIN INTERFACE=================================================================================
+//        Text
+        Text usernameText = new Text("USERNAME");
+        Text passwordText = new Text("PASSWORD");
+//        Textfields
+        TextField usernameTextField = new TextField();
+        PasswordField passwordTextField = new PasswordField();
+//        Buttons
+        Button loginButton = new Button("LOGIN");
+        Button registerButton = new Button("REGISTER");
+//        Creating the gridpane
+        GridPane loginGridpane = new GridPane();
+//      Setting up size of Grid pane
+        loginGridpane.setMinSize(600, 400);
+//      Setting the Padding
+        loginGridpane.setPadding(new Insets(10, 10, 10, 10));
+//      Setting the vertical and horizontal gaps between the columns
+        loginGridpane.setVgap(10);
+        loginGridpane.setHgap(10);
+//      Setting the grid alignment
+        loginGridpane.setAlignment(Pos.CENTER);
+//      Arranging all the nodes on the grid
+        loginGridpane.add(usernameText, 0, 0);
+        loginGridpane.add(passwordText, 0, 2);
+        loginGridpane.add(usernameTextField, 1, 0);
+        loginGridpane.add(passwordTextField, 1, 2);
+        loginGridpane.add(loginButton, 1, 4);
+        loginGridpane.add(registerButton, 1, 6);
+        Scene loginScene = new Scene(loginGridpane);
+        loginButton.setOnAction((e) -> {
+            farmer.Login(usernameTextField.getText(), passwordTextField.getText());
+            if (farmer.getStatus() == true) {
+                try {
+                    primarystage.setScene(testScene);
+                } catch (Exception ex) {
+                    ex.printStackTrace();
+                }
+                usernameTextField.clear();
+                passwordTextField.clear();
+            } else {
+
+            }
+        });
+        registerButton.setOnAction((e) -> {
+            primarystage.setScene(addUserScene);
+        });
+//      ========================END OF LOGIN INTERFACE=============================================================================
+        primarystage.setScene(loginScene);
         primarystage.show();
-   }
-   public static void main(String[] args){
-         launch(args);
-   }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
